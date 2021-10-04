b3e45889885e0a1a0520a739f0ae6a03dd19c165
palhaogv <palhaogv@gmail.com>
2021-10-04 12:40:30 +0200

    added by function

--- test.js
+++ test.js
@@ -3,4 +3,10 @@ function hello(name) {
     console.log('Hello ' + name);
 }
 
+function bye(name) {
+    // print hello plus person's name to screen
+    console.log('Goodbye ' + name);
+}
+
+bye('John');
 hello('Mark');
